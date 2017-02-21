b75cc11b02004d70f28822d1d265189f0ad59e89
Andreas Kempf <a.kempf@net-cry.de>
2017-02-21 11:07:42 +0100

    insert header

--- Windows Backup/status_wbackup.ps1
+++ Windows Backup/status_wbackup.ps1
@@ -1,4 +1,37 @@
+<#
+.SYNOPSIS
+  <Overview of script>
+  
+.DESCRIPTION
+  <Brief description of script>
+  
+.PARAMETER <Parameter_Name>
+    <Brief description of parameter input required. Repeat this attribute if required>
+    
+.INPUTS
+  <Inputs if any, otherwise state None>
+  
+.OUTPUTS
+  <Outputs if any, otherwise state None - example: Log file stored in C:\Windows\Temp\<name>.log>
+  
+.NOTES
+  Version:        1.0
+  Author:         <Name>
+  Creation Date:  <Date>
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  <Example goes here. Repeat this attribute for more than one example>
+
+  @author ${user}
+  @author ${name}
+  ${tags}
+ 
+#>
+$sScriptVersion = "1.0"
 $backupdata_array = Get-WBJob -previous 10
+$ts = New-TimeSpan -Days 1 -Hours 12 -Minutes 30
+$expired = (get-date) - $ts
 
 foreach($backupdata in $backupdata_array) {
     $JobType = $backupdata.JobType
@@ -21,8 +54,6 @@ foreach($backupdata in $backupdata_array) {
     }
 
 }
-$ts = New-TimeSpan -Days 1 -Hours 12 -Minutes 30
-$expired = (get-date) - $ts
 
 $endtime = [datetime]::parseexact($endtime, 'dd.MM.yyyy HH:mm',$null)
 
